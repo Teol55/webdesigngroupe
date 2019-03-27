98380ea4aab0b55691cb59dc116f65f67127cb3c
Arnaud <arnaud@MacBook-Pro-de-Arnaud.local>
2019-03-27 09:36:35 +0100

    Fix jQuery bug

--- user/themes/wdgdesign/js/app.js
+++ user/themes/wdgdesign/js/app.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 	$('.toggle_btn').click(function(){
 		$(this).toggleClass('open');
-		$('.nav').addClass('nav_open');
+		$('.navi').toggleClass('nav_open');
 	});
 });
