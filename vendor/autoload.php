70adaa6463664ad38fe81ddf53c6ce3514317726
C_MARTIN <m.ch55000@gmail.com>
2019-04-15 18:02:05 +0200

    upadte plugins and grav

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInit6be42a237d09ccd36b61fb06b93f9f7a::getLoader();
+return ComposerAutoloaderInitfd4a6be4e089d18689772fb27860fa0d::getLoader();
